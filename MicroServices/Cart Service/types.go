ad2ffdb1ca65fa54444c3d131f5326b37844d1a8
Shubham Sawant <shubham.sawant@sjsu.edu>
2018-12-29 09:37:41 -0800

    MicroServices added

---
+++ MicroServices/Cart Service/types.go
@@ -0,0 +1,17 @@
+package main
+
+import "gopkg.in/mgo.v2/bson"
+
+type Product struct {
+	ProductName string `bson:"ProductName" json:"ProductName"`
+	ProductDesc string `bson:"ProductDesc" json:"ProductDesc"`
+	Price       string `bson:"Price" json:"Price"`
+	Image       string `bson:"Image" json:"Image"`
+	Quantity    string `bson:"Quantity" json:"Quantity"`
+}
+
+type Cart struct {
+	CartID   bson.ObjectId `bson:"_id" json:"CartID"`
+	Total    string        `bson:"Total" json:"Total"`
+	Products []Product     `bson:"Products" json:"Products"`
+}
